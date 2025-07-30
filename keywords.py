cb587989dd31f85482d603977755b8b69a0d646b
pencil5611 <matthewdcook09@gmail.com>
2025-07-29 20:24:52 -0400

    Create keywords.py
    keywords for autocategorization

---
+++ keywords.py
@@ -0,0 +1,77 @@
+keyword_map = {
+    # Transportation
+    "uber": "transportation", "lyft": "transportation", "shell": "transportation", "chevron": "transportation",
+    "exxon": "transportation", "mobil": "transportation", "bp": "transportation", "sunoco": "transportation",
+    "wawa": "transportation", "76 gas": "transportation", "citgo": "transportation", "speedway": "transportation",
+    "metro": "transportation", "amtrak": "transportation", "transit": "transportation", "mta": "transportation",
+    "toll": "transportation", "parking": "transportation", "car wash": "transportation", "autozone": "transportation",
+    "advance auto": "transportation", "pep boys": "transportation", "valvoline": "transportation",
+
+    # Food & Dining
+    "mcdonald": "food", "burger king": "food", "wendy": "food", "starbucks": "food", "chipotle": "food",
+    "panera": "food", "chick-fil-a": "food", "taco bell": "food", "popeyes": "food", "subway": "food",
+    "panda express": "food", "five guys": "food", "domino": "food", "pizza hut": "food", "kfc": "food",
+    "dunkin": "food", "arbys": "food", "zaxby": "food", "sonic": "food", "jack in the box": "food",
+
+    # Groceries
+    "trader joes": "groceries", "whole foods": "groceries", "kroger": "groceries", "safeway": "groceries",
+    "walmart": "groceries", "aldi": "groceries", "costco": "groceries", "publix": "groceries", "giant": "groceries",
+    "h-e-b": "groceries", "stop & shop": "groceries", "winco": "groceries", "meijer": "groceries",
+    "ralphs": "groceries", "wegmans": "groceries", "vons": "groceries", "food lion": "groceries",
+
+    # Housing
+    "rent": "housing", "zillow": "housing", "airbnb": "housing", "mortgage": "housing", "landlord": "housing",
+    "property mgmt": "housing", "hoa": "housing", "apartment": "housing", "lease": "housing",
+
+    # Utilities
+    "coned": "utilities", "duke energy": "utilities", "comcast": "utilities", "xfinity": "utilities",
+    "spectrum": "utilities", "at&t": "utilities", "verizon": "utilities", "pg&e": "utilities", "dominion": "utilities",
+    "water": "utilities", "sewer": "utilities", "electric": "utilities", "natural gas": "utilities",
+
+    # Insurance
+    "geico": "insurance", "state farm": "insurance", "allstate": "insurance", "progressive": "insurance",
+    "usaa": "insurance", "liberty mutual": "insurance", "aetna": "insurance", "cigna": "insurance",
+    "anthem": "insurance", "blue cross": "insurance", "metlife": "insurance",
+
+    # Health & Medical
+    "cvs": "health", "walgreens": "health", "rite aid": "health", "pharmacy": "health", "doctor": "health",
+    "dental": "health", "vision": "health", "hospital": "health", "urgent care": "health", "clinic": "health",
+    "labcorp": "health", "quest": "health", "orthodontics": "health", "dermatology": "health",
+
+    # Personal Care
+    "sephora": "personal care", "ulta": "personal care", "barber": "personal care", "salon": "personal care",
+    "nails": "personal care", "haircut": "personal care", "spa": "personal care", "waxing": "personal care",
+    "massage": "personal care", "grooming": "personal care", "laser hair": "personal care",
+
+    # Entertainment
+    "netflix": "entertainment", "spotify": "entertainment", "hulu": "entertainment", "disney+": "entertainment",
+    "youtube premium": "entertainment", "game stop": "entertainment", "steam": "entertainment", "twitch": "entertainment",
+    "itunes": "entertainment", "fandango": "entertainment", "amc": "entertainment", "regal": "entertainment",
+    "movie": "entertainment", "concert": "entertainment", "eventbrite": "entertainment",
+
+    # Debts
+    "loan": "debts", "student loan": "debts", "credit card": "debts", "amex": "debts", "chase": "debts",
+    "capital one": "debts", "interest": "debts", "navient": "debts", "sofi": "debts", "discover": "debts",
+    "debt": "debts", "lendingclub": "debts",
+
+    # Subscriptions
+    "amazon prime": "subscriptions", "apple music": "subscriptions", "dropbox": "subscriptions", "google one": "subscriptions",
+    "nyt": "subscriptions", "washington post": "subscriptions", "substack": "subscriptions", "audible": "subscriptions",
+    "zoom": "subscriptions", "canva": "subscriptions", "notion": "subscriptions",
+
+    # Clothes & Shopping
+    "nike": "clothes", "adidas": "clothes", "h&m": "clothes", "zara": "clothes", "uniqlo": "clothes",
+    "old navy": "clothes", "gap": "clothes", "abercrombie": "clothes", "lululemon": "clothes", "shein": "clothes",
+    "asos": "clothes", "nordstrom": "clothes", "macys": "clothes",
+
+    # Investments
+    "robinhood": "investments", "fidelity": "investments", "vanguard": "investments", "etrade": "investments",
+    "td ameritrade": "investments", "charles schwab": "investments", "coinbase": "investments", "binance": "investments",
+    "sofi invest": "investments", "stash": "investments", "acorns": "investments",
+
+    # Miscellaneous / General Shopping
+    "amazon": "misc", "target": "misc", "paypal": "misc", "venmo": "misc",
+    "cashapp": "misc", "apple": "misc", "best buy": "misc", "ebay": "misc",
+    "home depot": "misc", "lowes": "misc", "ikea": "misc", "etsy": "misc",
+    "walmart.com": "misc", "bjs": "misc", "sams club": "misc", "chewy": "misc",
+    "petco": "misc", "petsmart": "misc"
